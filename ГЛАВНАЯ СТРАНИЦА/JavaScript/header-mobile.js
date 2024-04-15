0a011a32fe2e0957ee91db181304b23862cffc40
AlexandraDyachkova <131629780+AlexandraDyachkova@users.noreply.github.com>
2024-04-15 12:25:32 +0300

    15.04

--- ГЛАВНАЯ СТРАНИЦА/JavaScript/header-mobile.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menu__toggle').addEventListener('change', function() {
-    var headerNav = document.querySelector('.header__nav');
-    if (this.checked) {
-        headerNav.classList.add('menu-open');
-    } else {
-        headerNav.classList.remove('menu-open');
-    }
-});
