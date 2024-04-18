bd46fe3f504fca53c127610eb2198d7f8494f972
VlaDislav <92807808+kng111@users.noreply.github.com>
2024-04-18 14:05:50 +0300

    delete

--- IPM/ГЛАВНАЯ СТРАНИЦА/JavaScript/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Плавное появление текста
-    const mainText = document.querySelector(".main__text");
-    mainText.style.opacity = 1;
-
-    // Плавное появление изображения
-    const mainImage = document.querySelector(".main__image");
-    mainImage.style.transform = "translateX(0)";
-});
